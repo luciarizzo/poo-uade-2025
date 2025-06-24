87ead17b0bca96b903b5e3bf1a6c76686adfcd4a
luciarizzo <luciarizzodev@gmail.com>
2025-06-23 23:01:35 -0300

    primer parte main

--- src/org/example/Main.java
+++ src/org/example/Main.java
@@ -2,6 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Proyecto creado correctamente");
+        Hospital hospital = new Hospital("Hospital Dr Favaloro", "Avenida Salud 12345");
+
+        //Composición - un hospital CONTIENE areas:
+        Area pediatria = new Area("Pediatria", 2);
+        hospital.agregarArea(pediatria);
+
+        //Agregacion - el area tiene habitaciones
+        Habitacion h101 = new Habitacion(101, 2);
+        pediatria.agregarHabitaciones(h101);
+
+        System.out.println("Se creó el hospital " + hospital.getNombre() + " con el area " + pediatria.getNombre() + " y la habitacion " + h101.getNumero() + " con " + h101.getCamasDisponibles() + " camas disponibles.");
     }
 }
